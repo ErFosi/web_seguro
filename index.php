d998e7f1684774052b3c50112a8b6b0fc6324ed5
ErFosi <83069688+ErFosi@users.noreply.github.com>
2024-12-01 10:05:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,59 +1,46 @@
 <?php
 include 'db_connection.php';
 $conn = OpenCon();
-$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-
 
 function generateTableFromResult($result) {
-   $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
-   $html = '<table class="table table-striped">';
-   while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-        $html.="<tr>";
-	$html.="<td><a href='".$actual_link."/index.php?flight=".$row['FLIGHT_CODE']."'>".$row['FLIGHT_CODE']."</a></td>";
-        $html.="<td>".$row['SOURCE']."</td>";
-        $html.="<td>".$row['DESTINATION']."</td>";
-        $html.="<td>".$row['ARRIVAL']."</td>";
-        $html.="<td>".$row['DEPARTURE']."</td>";
-        $html.="<td>".$row['STATUS']."</td>";
-        $html.="<td>".$row['DURATION']."</td>";
-        $html.="<td>".$row['FLIGHTTYPE']."</td>";
-        $html.="<td>".$row['AIRLINED']."</td>";
-        $html.="</tr>\n\r";
-   }
-   $html.="</table>";
-   return $html;
+    $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
+    $html = '<table class="table table-striped">';
+    while ($row = $result->fetch_assoc()) {
+        $html .= "<tr>";
+        $html .= "<td><a href='" . htmlspecialchars($actual_link) . "/index.php?flight=" . htmlspecialchars($row['FLIGHT_CODE']) . "'>" . htmlspecialchars($row['FLIGHT_CODE']) . "</a></td>";
+        $html .= "<td>" . htmlspecialchars($row['SOURCE']) . "</td>";
+        $html .= "<td>" . htmlspecialchars($row['DESTINATION']) . "</td>";
+        $html .= "<td>" . htmlspecialchars($row['ARRIVAL']) . "</td>";
+        $html .= "<td>" . htmlspecialchars($row['DEPARTURE']) . "</td>";
+        $html .= "<td>" . htmlspecialchars($row['STATUS']) . "</td>";
+        $html .= "<td>" . htmlspecialchars($row['DURATION']) . "</td>";
+        $html .= "<td>" . htmlspecialchars($row['FLIGHTTYPE']) . "</td>";
+        $html .= "<td>" . htmlspecialchars($row['AIRLINED']) . "</td>";
+        $html .= "</tr>\n\r";
+    }
+    $html .= "</table>";
+    return $html;
 }
 
-?>
+$flightid = null;
 
-<body>
-<head>
-  <link rel="stylesheet" type="text/css" href="./css/css.css">
-  <link rel="stylesheet" href="./css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-<video autoplay muted loop id="myVideo">
-  <source src="./connections.mp4" type="video/mp4">
-</video>
+if (isset($_GET['flight']) && preg_match("/^[A-Za-z0-9]+$/", $_GET['flight'])) {
+    $flightid = $_GET['flight'];
+}
 
-<!-- Optional: some overlay text to describe the video -->
-<div class="content">
-<?php 
+if (!$flightid) {
+    $query = "SELECT * FROM FLIGHT LIMIT 10";
+    $stmt = $conn->prepare($query);
+} else {
+    $query = "SELECT * FROM FLIGHT WHERE FLIGHT_CODE = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("s", $flightid);
+}
 
-    if(!isset($_GET['flight']))
-    {
-      $query = "SELECT * FROM FLIGHT LIMIT 10";
-    } else { 
-      $flightid = $_GET['flight'];
-      $flightid = "$flightid";
-      $query = "SELECT * FROM FLIGHT WHERE FLIGHT_CODE = '$flightid'";
-    }
+$stmt->execute();
+$result = $stmt->get_result();
 
-
-$result = mysqli_query($conn,$query) or die(mysqli_error());
-
-
-echo generateTableFromResult($result); 
-
+echo generateTableFromResult($result);
 ?>
 
 </div>
